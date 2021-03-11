2ad0dcdb4dacffa492842aaf7a2208cff94b5d07
mohit <mohitklamba@gmail.com>
2021-03-11 20:07:55 +0530

    static covid tracker

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-function updateMap ()
-{
-    fetch("/data.json")
-    .then(response => response.json())
-    .then(rsp => {
-        console.log(rsp.data)
-        rsp.data.forEach(element => {
-            latitude=element.latitude;
-            longitude=element.longitude;
-            
-        });
-    })
+function updateMap() {
+  console.log("showing realtime data");  
 
+  fetch("/data.json")
+    .then((response) => response.json())
+    .then((rsp) => {
+      console.log(rsp.data);
+      rsp.data.forEach((element) => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+
+        cases=element.infected;
+        if(cases>255){
+            color= "rgb(255,0,0)"
+        }
+        else {
+            color=`rgb(${cases},0,0)`;
+        }
+
+        new mapboxgl.Marker({
+          draggable: false,
+          color:color
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+      });
+    });
 }
+let interval=2000;
+setInterval(updateMap,interval);
 
-
-updateMap();
